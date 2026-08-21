fb97ad9479c689d19c3dc0eb2304d3de6f15bf2e
jesusrivasmiranda24-ux <jesusrivasmiranda24@gmail.com>
2026-08-21 14:03:55 -0500

    Add files via upload

---
+++ CalcularAreaCirculo.cs.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Text.RegularExpressions;
+
+namespace program
+{
+    internal class Program
+    {
+
+        static double CalcularAreaCirculo(double radio)
+        {
+            const double PI = 3.141592;
+            //ahora voy a realizar el calculo con la forma 
+            double area = PI * Math.Pow(radio, 2);
+            return area;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("ingresar el radio del circulo: ");
+    double r = Convert.ToDouble(Console.ReadLine());
+
+
+            double result = CalcularAreaCirculo(r);
+            Console.WriteLine($"El area es: {result:f2}");
+
+
+        }
+
+    }
+}
