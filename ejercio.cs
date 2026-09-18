20df1e261c890ff04780de1b757dc38dc022ca4e
jesusrivasmiranda24-ux <jesusrivasmiranda24@gmail.com>
2026-09-18 13:09:41 -0500

    Create ejercio.cs
    Added a new C# program that initializes an array and prints the first element.

---
+++ ejercio.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numero = new int[5]
+            { 20, 50, 29, 99, -3 };
+            Console.WriteLine( $"elemento e [0]: {numero[0]}");
+        }
+    }
+}
