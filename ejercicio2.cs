5e4fea983863a2796db08f65e8af8181de8a30a4
jesusrivasmiranda24-ux <jesusrivasmiranda24@gmail.com>
2026-09-18 13:25:53 -0500

    Create ejercicio2.cs
    Joimer Rivas

---
+++ ejercicio2.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] estudiantes =
+            { "ana", "nico", "anuar", "stevel" };
+
+            for (int i = 0; i < estudiantes.Length; i++)
+            {
+                Console.WriteLine(
+                    $"Indice [{i}] {estudiantes[i]}");
+
+            }
+
+        }
+
+    }
+
+}
+            
+
